bbb13a5211e3ea1115517f265971d6acde1156db
Ayako Nakamoto <87840289+AYKWORKS@users.noreply.github.com>
2022-07-09 13:01:56 +0900

    テンプレート文字列練習

--- src/index.js
+++ src/index.js
@@ -1,22 +1,31 @@
 /*
  * coonst,letの練習
  */
-const val3 = "const定数";
-console.log(val3);
+// const val3 = "const定数";
+// console.log(val3);
 
-const val4 = {
-  name: "ayk",
-  age: 1
-};
+// const val4 = {
+//   name: "ayk",
+//   age: 1
+// };
+// // console.log(val4);
+// val4.name = "ay";
+// val4.address = "kumamoto";
 // console.log(val4);
-val4.name = "ay";
-val4.address = "kumamoto";
-console.log(val4);
 
-// 配列
-const val5 = ["dog", "cat"];
+// // 配列
+// const val5 = ["dog", "cat"];
+// // console.log(val5);
+// val5[0] = "bird";
+// // console.log(val5);
+// val5.push("monky");
 // console.log(val5);
-val5[0] = "bird";
-// console.log(val5);
-val5.push("monky");
-console.log(val5);
+
+/**
+ * テンプレート文字列
+ * テンプレートリテラル ``
+ */
+const name = "あや";
+const age = 112;
+const message1 = `私の名前は${name}です。年齢は${age}です。`;
+console.log(message1);
